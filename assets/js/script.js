a051247cd638f9d60c1907e9370c30da691385e1
henilcodes <henilcode@gmail.com>
2023-03-26 09:39:02 +0530

    remove some image and one header for all page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
   $.ajax({
+    url: "master/header.html",
+    context: document.body,
+  }).done(function (data) {
+    $("#header").html(data);
+  });
+  $.ajax({
     url: "master/footer.html",
     context: document.body,
   }).done(function (data) {
     $("#footer").html(data);
   });
-
-  // $.ajax({
-  //   url: "master/header.html",
-  //   context: document.body,
-  // }).done(function (data) {
-  //   $("#header").html(data);
-  // });
 });
